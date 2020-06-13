ded2e7e9d5977898cd1d0076dfc9e4a08706ccdd
Daniel Collings <danielcollings@Daniels-MacBook-Pro.local>
2020-06-13 13:35:28 +0100

    return object formatted

--- api/main.go
+++ api/main.go
@@ -19,27 +19,10 @@ func main(){
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
 
-
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
     (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
     (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-}
-
-func defaultHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "hello world")
-}
-
-func radiusHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "hello radius")
-}
-
-func locationHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "hello location")
-}
-
-type Users struct{
-	Users []User `json:"users"`
 }
 
 type User struct{
@@ -48,8 +31,13 @@ type User struct{
 	Last_name string `json:"last_name"`
 	Email string `json:"email"`
 	Ip_address string `json:"ip_address"`
-	Longitude string `json:"longitude"`
-	Latitude string `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Latitude float64 `json:"latitude"`
+	Distance float64 `json:"distance"`
+}
+
+type Users struct{
+	Users []User
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
@@ -68,9 +56,11 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
         log.Fatalln(err)
 	}
 
-	var users Users
+	data := []byte(body)
+	keys :=  make([]User,0)
+	json.Unmarshal(data, &keys)
 
-	json.Unmarshal(body, users)
+	// fmt.Println(keys)
 
 	// var arr =  data.filter(function(person) {
     //     var distance = GetDistanceBetweenTwoCoordinates(51.509865, -0.118092, person.latitude, person.longitude);
@@ -82,29 +72,9 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	// s := []Users
     // for key := range users User {
     //     fmt.Println(key)
-    // }
-
-
-	
-	json.NewEncoder(w).Encode(string(body))
+	// }
+	json.NewEncoder(w).Encode(keys)
 }
-
-// func FilterMagicalCreatures(record string) bool {
-// 	magicalCreatures := []string{
-// 		"Unicorn",
-// 		"Dragon",
-// 		"Griffin",
-// 		"Minotaur",
-// 	}
-
-// 	for _, c := range magicalCreatures {
-// 		if record == c {
-// 			return false
-// 		}
-// 	}
-
-// 	return true
-// }
 
 func GetDistanceBetweenTwoCoordinates(lat1 float64,lon1 float64,lat2 float64,lon2 float64) float64{
 	var radius float64 = 6371;
